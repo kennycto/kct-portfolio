8e8cf57308765f0ca6fa1845581f07835cc0def3
Kenny To <kenny.to7@wipro.com>
2020-02-10 00:38:29 -0800

    Updates

---
+++ precache-manifest.24f888ab5aec81284b625dbf8d260b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91490f867a0c96628a155bac8da33ba8",
+    "url": "/kct-portfolio/index.html"
+  },
+  {
+    "revision": "f863887c8c64d7632928",
+    "url": "/kct-portfolio/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "d459c3fae0d7bc953c00",
+    "url": "/kct-portfolio/static/css/main.9078fad5.chunk.css"
+  },
+  {
+    "revision": "f863887c8c64d7632928",
+    "url": "/kct-portfolio/static/js/2.d4e0cbb8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/kct-portfolio/static/js/2.d4e0cbb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d459c3fae0d7bc953c00",
+    "url": "/kct-portfolio/static/js/main.9c153358.chunk.js"
+  },
+  {
+    "revision": "08b96662765b5e735a6d",
+    "url": "/kct-portfolio/static/js/runtime-main.aa67c6c7.js"
+  }
+]);
