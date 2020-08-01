bdce21debf685b96673e859a7af9c20e469b9cb0
Kenny To <knyto2@gmail.com>
2020-08-01 16:30:32 -0700

    Updates

---
+++ precache-manifest.2effe69b2d6587f188e191d7437536b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce22d29114f6b6007718bf8978f29ac",
+    "url": "/kct-portfolio/index.html"
+  },
+  {
+    "revision": "a8b896882f0d670784a2",
+    "url": "/kct-portfolio/static/css/main.ed144c29.chunk.css"
+  },
+  {
+    "revision": "44e80c84965efab0885c",
+    "url": "/kct-portfolio/static/js/2.e2a7f974.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kct-portfolio/static/js/2.e2a7f974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b896882f0d670784a2",
+    "url": "/kct-portfolio/static/js/main.5358c0ce.chunk.js"
+  },
+  {
+    "revision": "4da82aea000dac4dd7d0",
+    "url": "/kct-portfolio/static/js/runtime-main.be2709aa.js"
+  }
+]);
