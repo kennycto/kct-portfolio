c348e3466d21cd7b90b8603f44eb7f45a7d99fef
Kenny To <knyto2@gmail.com>
2020-08-08 16:29:15 -0700

    format app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React from "react";
-import "./App.scss";
+import React from 'react';
+import './App.scss';
 
-import { connect } from "react-redux";
-import { ProfileActions } from "./store/actions/index";
+import { connect } from 'react-redux';
+import { ProfileActions } from './store/actions/index';
 
-import Nav from "./components/Nav";
-import UserHeader from "./components/UserHeader";
-import Pages from "./pages";
+import Nav from './components/Nav';
+import UserHeader from './components/UserHeader';
+import Pages from './pages';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <Nav />
-                <UserHeader />
-                <Pages />
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.props.getProfile();
+  }
 
-    componentDidMount() {
-        this.props.getProfile();
-    }
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <UserHeader />
+        <Pages />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-    };
+  return {
+    user: state.user,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getProfile: () => dispatch(ProfileActions.getProfile()),
-    };
+  return {
+    getProfile: () => dispatch(ProfileActions.getProfile()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
